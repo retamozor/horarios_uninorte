8a000efa680a00cf289b022767131581ec87d896
Rafael-Conrado <retamozor@uninorte.edu.co>
2023-12-31 16:59:24 -0500

    axios interceptors

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,10 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://mihorario.uninorte.edu.co/M1H0R4R10',
-  headers: {
-    Authorization: `bearer ${JSON.parse(localStorage.getItem('horun') ?? '{"token": ""}').token}`
-  },
-})
+	baseURL: "https://mihorario.uninorte.edu.co/M1H0R4R10",
+});
 
-export default api
+api.interceptors.request.use(
+	config => {
+		const token = JSON.parse(
+			localStorage.getItem("horun") ?? '{"token": ""}'
+		).token;
+    
+		if (token) config.headers.Authorization = `Bearer ${token}`;
+		return config;
+	},
+	error => Promise.reject(error)
+);
+
+export default api;
--- tampermonkey.js
+++ tampermonkey.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Mi Horario Uninorte
 // @namespace    https://github.com/retamozor/horarios_uninorte/
-// @version      0.3
+// @version      0.4
 // @description  Genera tu horario uninorte
 // @author       Rafael
 // @match        https://pomelo.uninorte.edu.co/mihorario/*
-// @require	     https://github.com/retamozor/horarios_uninorte/raw/main/dist/assets/index-Wsf_V7Sd.js
+// @require	     https://github.com/retamozor/horarios_uninorte/raw/main/dist/assets/index-ruooUYdo.js
 // @resource     AppStyles https://github.com/retamozor/horarios_uninorte/raw/main/dist/assets/index-lHvq-fct.css
 // @updateURL    https://github.com/retamozor/horarios_uninorte/raw/main/tampermonkey.js
 // @downloadURL  https://github.com/retamozor/horarios_uninorte/main/tampermonkey.js
