92b744b0054c47f24a1e09418233757b6a4f738d
Rafael-Conrado <retamozor@uninorte.edu.co>
2024-01-14 11:19:16 -0500

    remove router

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const id = "root2";
 let root = document.getElementById(id);
@@ -12,20 +11,9 @@ if (root === null) {
 	document.body.appendChild(root);
 }
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <></>,
-    errorElement: <></>
-  },
-  {
-    path: "/mihorario/inicio",
-    element: <App />,
-  },
-]);
 
 ReactDOM.createRoot(root).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<App />
 	</React.StrictMode>
 );
--- tampermonkey.js
+++ tampermonkey.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Mi Horario Uninorte
 // @namespace    https://github.com/retamozor/horarios_uninorte
-// @version      1.2
+// @version      1.3
 // @description  Genera tu horario uninorte
 // @author       Rafael
 // @match        https://pomelo.uninorte.edu.co/mihorario/*
-// @require	     https://github.com/retamozor/horarios_uninorte/raw/main/dist/assets/index-nawsdrBH.js 
+// @require	     https://github.com/retamozor/horarios_uninorte/raw/main/dist/assets/index-F7cmZiug.js 
 // @resource     AppStyles https://github.com/retamozor/horarios_uninorte/raw/main/dist/assets/index-ioUoTLU7.css
 // @updateURL    https://github.com/retamozor/horarios_uninorte/raw/main/tampermonkey.js
 // @downloadURL  https://github.com/retamozor/horarios_uninorte/raw/main/tampermonkey.js
